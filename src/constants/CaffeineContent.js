5d9c657503a699307f98cce892ecc40b670b0fc2
Davood <davood.akrami.rk2025@gmail.com>
2025-03-06 15:49:54 +0330

    Writing the code of the categories and CoffeeContent files and change the products file based on them

---
+++ src/constants/CaffeineContent.js
@@ -0,0 +1,35 @@
+const caffeineContent_strong_name = "قوی";
+const caffeineContent_medium_name = "متوسط";
+const caffeineContent_weak_name = "ضعیف";
+
+const caffeineContent_strong_id = "strong";
+const caffeineContent_medium_id = "medium";
+const caffeineContent_weak_id = "weak";
+
+
+const caffeineContent = [
+    {
+        name: caffeineContent_strong_name,
+        id: caffeineContent_strong_id,
+    },
+    {
+        name: caffeineContent_medium_name,
+        id: caffeineContent_medium_id,
+    },
+    {
+        name: caffeineContent_weak_name,
+        id: caffeineContent_weak_id,
+    }
+]
+
+export {
+    caffeineContent_strong_name,
+    caffeineContent_medium_name,
+    caffeineContent_weak_name,
+    caffeineContent_strong_id,
+    caffeineContent_medium_id,
+    caffeineContent_weak_id,
+    caffeineContent,
+}
+
+export default caffeineContent;
---
+++ src/constants/categories.js
@@ -0,0 +1,35 @@
+const category_coffeebeans_name = "دانه قهوه";
+const category_instantcoffee_name = "قهوه خشک"; 
+const category_groundcoffee_name = "قهوه آسیاب شده";
+
+const category_coffeebeans_id = "coffee-beans";
+const category_instantcoffee_id = "instant-coffee";
+const category_groundcoffee_id = "ground-coffee";
+
+
+const categories = [
+    {
+        name: category_coffeebeans_name,
+        id: category_coffeebeans_id,
+    },
+    {
+        name: category_instantcoffee_name,
+        id: category_instantcoffee_id,
+    },
+    {
+        name: category_groundcoffee_name,
+        id: category_groundcoffee_id,
+    }
+]
+
+export {
+    category_coffeebeans_name,
+    category_instantcoffee_name,
+    category_groundcoffee_name,
+    category_coffeebeans_id,
+    category_instantcoffee_id,
+    category_groundcoffee_id,
+    categories,
+}
+
+export default categories;
